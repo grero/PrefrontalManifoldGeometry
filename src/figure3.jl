e09f82ab19120e83d9fc83dc2cd024fe9e2d100b
Roger Herikstad <roger.herikstad@gmail.com>
2023-05-25 16:20:16 +0800

    Adds code related to figure 3

---
+++ src/figure3.jl
@@ -0,0 +1,320 @@
+using Makie
+using CairoMakie
+using JLD2
+
+include("utils.jl")
+include("regression.jl")
+include("trajectories.jl")
+include("plot_utils.jl")
+
+"""
+Data for the figure comparing path length, average speed and post-cue period in terms of
+how much reaction time variance they explain
+"""
+function get_reaction_time_regressors(;redo=false, do_save=true,area="FEF")
+    # TODO: Check why the r² values went down. I think it was after I changed from summing up the squares
+    # .     to summing about the square roots.
+    if area == :FEF
+        fname = "reaction_time_predictors_new.jld2"
+    else
+        fname = "reaction_time_predictors_new_$area.jld2"
+    end
+    if isfile(fname) && !redo
+        stats_results = JLD2.load(fname, "stats_results")
+    else
+        stats_results = Dict{String,Any}()
+        stats_results["path_length"] = Dict{String,Any}()
+        stats_results["path_length2"] = Dict{String,Any}()
+        stats_results["avg_speed"] = Dict{String,Any}()
+        stats_results["delay2"] = Dict{String,Any}()
+        stats_results["postcue"] = Dict{String,Any}()
+        stats_results["path_length"]["W"] = Dict{String,Any}(zip(["S","lrt","tridx"], get_path_length_and_rtime("W", 85.0, 35.0;area=area)))
+        stats_results["path_length"]["J"] = Dict{String,Any}(zip(["S","lrt", "tridx"], get_path_length_and_rtime("J", 85.0, 35.0;area=area)))
+        stats_results["path_length2"]["W"] = Dict{String,Any}(zip(["S","lrt","tridx"], get_path_length_and_rtime("W", 85.0, 35.0, operation=:path_length2,area=area)))
+        stats_results["path_length2"]["J"] = Dict{String,Any}(zip(["S","lrt", "tridx"], get_path_length_and_rtime("J", 85.0, 35.0, operation=:path_length2, area=area)))
+        stats_results["avg_speed"]["W"] = Dict{String,Any}(zip(["S", "lrt", "tridx"], get_path_length_and_rtime("W", 85.0, 35.0,operation=:mean_speed, area=area)))
+        stats_results["avg_speed"]["J"] = Dict{String,Any}(zip(["S","lrt", "tridx"], get_path_length_and_rtime("J", 85.0, 35.0,operation=:mean_speed, area=area)))
+        stats_results["delay2"]["W"] = Dict{String,Any}(zip(["lrt","S", "tridx"], explain_rtime_variance("W", :cue;realign=true, area=area)))
+        stats_results["delay2"]["J"] = Dict{String,Any}(zip(["lrt","S", "tridx"], explain_rtime_variance("J", :cue;realign=true,area=area)))
+        stats_results["postcue"]["W"] = Dict{String,Any}(zip(["lrt","S","tridx"], explain_rtime_variance("W", :cue;realign=true, reg_window=(0.0, 50.0), area=area)))
+        #TODO: Why are so many trials excluded here?
+        stats_results["postcue"]["J"] = Dict{String,Any}(zip(["lrt","S", "tridx"],explain_rtime_variance("J", :cue;realign=true, reg_window=(0.0, 50.0), area=area)))
+      
+        if any(isnan.(stats_results["path_length"]["W"]["S"]))
+            @warn "NaN encountered"
+        end
+        # make sure we are using the same indices
+        k1 = "postcue"
+        k2 = "path_length"
+        tridxj = intersect(stats_results[k1]["J"]["tridx"],
+                stats_results[k2]["J"]["tridx"])
+        tridxw = intersect(stats_results[k1]["W"]["tridx"],
+                stats_results[k2]["W"]["tridx"])
+        tridxpcj = findall(in(tridxj), stats_results[k1]["J"]["tridx"])
+        tridxplj = findall(in(tridxj), stats_results[k2]["J"]["tridx"])
+        tridxpcw = findall(in(tridxw), stats_results[k1]["W"]["tridx"])
+        tridxplw = findall(in(tridxw), stats_results[k2]["W"]["tridx"])
+
+        for (kk,_tridxjw) in zip(["path_length","path_length2","avg_speed","postcue"],[(tridxplj,tridxplw),(tridxplj,tridxplw),(tridxplj,tridxplw),(tridxpcj, tridxpcw)])
+            for (subject,_tridx) in zip(["J","W"], _tridxjw) 
+                @debug subject kk
+                xj = stats_results[kk][subject]
+                @debug size(xj["S"]) size(xj["lrt"]) size(xj["tridx"])
+                if size(xj["S"]) == (0,)
+                    error("No trials found. subject=$(subject) kk=$(kk)")
+                end
+                xj["S"] = xj["S"][_tridx]
+                xj["lrt"] = xj["lrt"][_tridx]
+                xj["tridx"] = xj["tridx"][_tridx]
+            end
+        end
+        
+        for (k,v) in stats_results
+            xj = v["J"]
+            xw = v["W"]
+            lrt = [xw["lrt"];xj["lrt"]]
+            S = cat(xw["S"],xj["S"],dims=1)
+            idx = isfinite.(S)
+            S = repeat(S[idx],1,1)
+            v["both"] = Dict{String,Any}()
+            vb = v["both"]
+            @show k size(S) size(lrt[idx])
+            vb["β"], vb["r²"], vb["pv"], vb["rss"] = llsq_stats(S, lrt[idx])
+            vb["lrt"], vb["S"] = (lrt[idx], S)
+            # regress individual monkeys too
+            idxj = isfinite.(xj["S"])
+            xj["β"], xj["r²"], xj["pv"], xj["rss"] = llsq_stats(repeat(xj["S"][idxj],1,1), xj["lrt"][idxj])
+            idxw = isfinite.(xw["S"])
+            xw["β"], xw["r²"], xw["pv"], xw["rss"] = llsq_stats(repeat(xw["S"][idxw],1,1), xw["lrt"][idxw])
+            # for nested regression, simply add one variable to the other
+        end
+        # now do hierarchial regression
+        for (k1, k2) in [("postcue","path_length"),("path_length","postcue"),("postcue","path_length2"),("path_length2","postcue")]
+            kk = "$(k1)→$(k2)"
+            stats_results[kk] = Dict{String,Any}()
+            for subject in ["J","W"]
+                stats_results[kk][subject] = Dict{String, Any}()
+                qq = stats_results[kk][subject]
+                Splw = stats_results[k2][subject]["S"]
+                Spcw = stats_results[k1][subject]["S"]
+                lrtplw = stats_results[k2][subject]["lrt"]
+                lrtpcw = stats_results[k1][subject]["lrt"]
+                @assert lrtplw == lrtpcw
+                S = [Spcw Splw]
+                lrt = lrtpcw
+                qqidx = isfinite.(dropdims(sum(S,dims=2),dims=2))
+                # residual
+                _β = stats_results[k2][subject]["β"]
+                rr = Splw*_β[1:end-1]' .+ _β[end] - lrtpcw
+                βr, r²r, pvr, rssr = llsq_stats(repeat(Spcw[qqidx],1,1), rr[qqidx])
+                qq["r²_res"] = r²r
+                β2,r², pv,rss2 = llsq_stats(S[qqidx,:], lrt[qqidx])
+                p2 = length(β2)
+                β1,_,_,rss1 =  llsq_stats(S[qqidx,1:1], lrt[qqidx])
+                p1 = length(β1)
+                n = length(lrt)
+                F = (rss1 - rss2)/(p2-p1)
+                F /= rss2/(n-p2)
+                pv = 1.0 - cdf(FDist(p2-p1, n-p2), F)
+                qq["β"] = β2
+                qq["β1"] = β1
+                qq["rss"] = rss2
+                qq["rss1"] = rss1
+                qq["pv"] = pv
+                qq["F"] = F
+                qq["r²"] = r²
+            end
+            # both
+            stats_results[kk]["both"] = Dict{String, Any}()
+            qq = stats_results[kk]["both"]
+            Splw = stats_results[k2]["W"]["S"]
+            Splj = stats_results[k2]["J"]["S"]
+            Spcw = stats_results[k1]["W"]["S"]
+            Spcj = stats_results[k1]["J"]["S"]
+
+            lrtplw = stats_results[k2]["W"]["lrt"]
+            lrtplj = stats_results[k2]["J"]["lrt"]
+            lrtpcw = stats_results[k1]["W"]["lrt"]
+            lrtpcj = stats_results[k1]["J"]["lrt"]
+
+            @assert lrtplj == lrtpcj
+            S = [[Spcw;Spcj] [Splw;Splj]]
+            lrt = [lrtplw;lrtplj]
+            qqidx = isfinite.(dropdims(sum(S,dims=2),dims=2))
+            β2,r², pv,rss2 = llsq_stats(S[qqidx,:], lrt[qqidx])
+            p2 = length(β2)
+            β1,_,_,rss1 =  llsq_stats(S[qqidx,1:1], lrt[qqidx])
+            p1 = length(β1)
+            n = length(lrt)
+            F = (rss1 - rss2)/(p2-p1)
+            F /= rss2/(n-p2)
+            pv = 1.0 - cdf(FDist(p2-p1, n-p2), F)
+            qq["β"] = β2
+            qq["β1"] = β1
+            qq["rss"] = rss2
+            qq["rss1"] = rss1
+            qq["pv"] = pv
+        end
+        #now do residual
+
+        stats_results["path_length"]["legend_anchor"] = (0.45, 0.85)
+        stats_results["path_length2"]["legend_anchor"] = (0.45, 0.85)
+        stats_results["avg_speed"]["legend_anchor"] = (0.45, 0.85)
+        stats_results["delay2"]["legend_anchor"] = (0.45, 0.85)
+        stats_results["postcue"]["legend_anchor"] = (0.45, 0.85)
+        if do_save
+            JLD2.save(fname, Dict("stats_results"=>stats_results))
+        end
+    end
+    stats_results
+end
+
+"""
+Plot trajectory illustration using the supplied axis `ax`
+"""
+function plot_trajectory_illustration!(lg)
+    nn = 500
+    θ = range(0.0, stop=π, length=nn)
+    a = 1.0
+    b1 = 1.5
+    b2 = 1.1
+    traj1 = [a*cos.(θ) b1*sin.(θ)]
+    traj2 = [a*cos.(θ) b2*sin.(θ)]
+    traj3 = traj2 .+ [0.0 0.1]
+    dm,idx1 = compute_triangular_path_length(traj1)
+    dm,idx2 = compute_triangular_path_length(traj2)
+    tidx1 = [1, idx1, idx1, nn]
+    tidx2 = [1, idx2, idx2, nn]
+    ss = 40 
+    l2 = sqrt.(sum(abs2, diff(traj2,dims=1),dims=2))
+    delta = sum(l2[1:ss])
+    with_theme(theme_minimal()) do
+        ax = Axis(lg[1,1], autolimitaspect=1)
+        ax2 = Axis(lg[1,3], autolimitaspect=1)
+        linkaxes!(ax, ax2)
+        lines!(ax, traj1[:,1], traj1[:,2],color="red")
+        linesegments!(ax, traj1[tidx1,1], traj1[tidx1,2],color="red", linestyle=:dot)
+        plot_normals!(ax, traj1;ss=(traj,i)->advance(traj,i,delta), linelength=0.16,color="red")
+        lines!(ax, traj2[:,1], traj2[:,2],color="blue")
+        linesegments!(ax, traj2[tidx2,1], traj2[tidx2,2],color="blue",linestyle=:dot)
+        plot_normals!(ax, traj2;ss=(traj,i)->advance(traj,i,delta), linelength=0.16,color="blue")
+    
+        lines!(ax2, traj2[:,1], traj2[:,2],color="blue", label="Short RT")
+        plot_normals!(ax2, traj2;ss=(traj,i)->advance(traj,i,delta), linelength=0.16,color="blue")
+    
+        lines!(ax2, traj3[:,1], traj3[:,2],color="red", label="Long RT")
+        plot_normals!(ax2, traj3;ss=(traj,i)->advance(traj,i,delta/2.0), linelength=0.16,color="red")
+        scatter!(ax2, traj2[[1,nn],1], traj2[[1,nn],2], marker=:rect, markersize=20px, color=[RGB(0.5, 1.0, 1.0),RGB(0.5, 1.0, 0.5)])
+        scatter!(ax, traj2[[1,nn],1], traj2[[1,nn],2], marker=:rect, markersize=20px, color=[RGB(0.5, 1.0, 1.0),RGB(0.5, 1.0, 0.5)])
+        ll = Legend(lg[1,2], ax2, valign=:top,framevisible=true, padding=5.0, labelsize=12)
+
+        text!(ax, 0.5, 0.3, text="Same speed\nDifferent length", space=:relative, align=(:center, :top), fontsize=12)
+        text!(ax2, 0.5, 0.3, text="Same length\nDifferent speed", space=:relative, align=(:center, :top), fontsize=12)
+        colgap!(lg,1,0.1)
+        colgap!(lg,2,0.1)
+        for _ax in [ax, ax2]
+            _ax.xticks = (traj2[[nn,1],1],["Go-cue","Threshold"])
+            _ax.yticksvisible = false
+            _ax.xticklabelsize=12
+            _ax.yticklabelsvisible = false
+            _ax.bottomspinevisible = false
+            _ax.leftspinevisible = false
+        end
+    end 
+end
+
+function plot()
+    stats_results = get_reaction_time_regressors(;redo=false, do_save=true)
+    qq1 = stats_results["postcue→path_length2"]["J"]
+    qq2 = stats_results["path_length2→postcue"]["J"]
+
+    qq1 = stats_results["postcue→path_length2"]["W"]
+    qq2 = stats_results["path_length2→postcue"]["W"]
+    # tweak to make sure we can see the annotations
+    # tweak to make sure we can see the annotations
+    ymin = -0.3
+    ymax = 0.8
+    with_theme(plot_theme) do
+        markersize=5px
+        fig = Figure(resolution=(800,600))
+        axes = [Axis(fig[2,i]) for i in 1:3]
+        #linkyaxes!(axes...)
+        for (ax, vv) in zip(axes, ["postcue", "path_length2","avg_speed"])
+            _data = stats_results[vv]
+            r² = _data["both"]["r²"]
+            β = _data["both"]["β"]
+            pv = _data["both"]["pv"]
+            r²j = _data["J"]["r²"]
+            pvj = _data["J"]["pv"]
+            βj = _data["J"]["β"]
+            r²w = _data["W"]["r²"]
+            pvw = _data["W"]["pv"]
+            βw = _data["W"]["β"]
+            lstring = [L"$r^2 = %$(round(_r², sigdigits=2))$, $p = $%$(round(_pv,sigdigits=2))" for (_r², _pv) in zip([r², r²w, r²j],[pv, pvw, pvj])]
+            S = stats_results[vv]["both"]["S"]
+            lrt = stats_results[vv]["both"]["lrt"]
+            Sw = stats_results[vv]["W"]["S"]
+            Sj = stats_results[vv]["J"]["S"]
+            scatter_color = fill(ax.palette.color[][1], length(S))
+            scatter_color[length(Sw)+1:end] .= ax.palette.color[][2]
+            scatter!(ax, S[:], lrt, color=scatter_color, markersize=markersize)
+            if "legend_anchor" in keys(stats_results[vv])
+                _anchor = stats_results[vv]["legend_anchor"]
+            else
+                _anchor = (0.45, 0.85)	
+            end
+            text!(ax, _anchor[1]-0.04, _anchor[2]+0.1, text=lstring[1], space=:relative, fontsize=12)
+            text!(ax, _anchor[1]-0.04, _anchor[2]+0.05, text=lstring[2], space=:relative, color=ax.palette.color[][1], fontsize=12)
+            text!(ax, _anchor[1]-0.04, _anchor[2], text=lstring[3], space=:relative, color=ax.palette.color[][2], fontsize=12)
+            ablines!(ax, β[2:2], β[1:1], color="black", linestyle=:dot)
+            ablines!(ax, βw[2:2], βw[1:1], color=Cycled(1), linestyle=:dot)
+            ablines!(ax, βj[2:2], βj[1:1], color=Cycled(2), linestyle=:dot)
+            ylims!(ax, ymin, ymax)
+        end
+        # joint vs single regression
+        lg7 = GridLayout()
+        fig[1,3] = lg7
+        axesp = [Axis(lg7[1,i]) for i in 1:2]
+        linkyaxes!(axesp...)
+        for (ii,(subject,ax)) in enumerate(zip(["W","J"], axesp))
+            qq = stats_results["postcue→path_length2"][subject]
+            rsstot = qq["rss"]
+            r² = qq["r²"]
+            r²1 = stats_results["postcue"][subject]["r²"] 
+            r²2 = stats_results["path_length2"][subject]["r²"]
+            r²1res = stats_results["postcue→path_length2"][subject]["r²_res"]
+            r²2res = stats_results["path_length2→postcue"][subject]["r²_res"]
+            barplot!(ax, 1:3, [r²1, r²2, r²], color=ax.palette.color[][ii])
+            hlines!(ax, r²1+r²2, color="black", linestyle=:dot)
+            ax.xticks=([1,2,3], ["MP", "PL", "MP+PL"])
+            ax.xticklabelrotation = π/2
+        end
+        axesp[1].ylabel = "r²"
+        axesp[2].yticklabelsvisible = false
+        axesp[2].yticksvisible = false
+        axesp[2].leftspinevisible = false
+
+        # add trajectory illustration
+        lg4 = GridLayout()
+        fig[1,1:2] = lg4
+        plot_trajectory_illustration!(lg4)
+        labels = [Label(fig[1,1,TopLeft()], "A"),
+                Label(fig[2,1,TopLeft()], "B"),
+                Label(fig[2,2,TopLeft()], "C"),
+                Label(fig[2,3,TopLeft()], "D"),
+                Label(fig[1,3,TopLeft()], "E")]
+        ax =axes[1]
+        ax.ylabel = "log(rt)"
+        ax.xlabel = "Motor prep (MP)"
+        ax = axes[2]
+        ax.yticklabelsvisible = false
+        ax.xlabel = "Path length (PL)"
+        ax = axes[3]
+        ax.yticklabelsvisible = false
+        ax.xlabel = "Avg speed (AS)"
+        rowsize!(fig.layout,1,Relative(0.25))
+        fname = joinpath("figures","manuscript","reaction_time_regressors_new.pdf")
+        save(fname, fig,pt_per_unit=1)
+        fig
+    end
+end
---
+++ src/trajectories.jl
@@ -0,0 +1,194 @@
+"""
+```julia
+function get_normals(traj::Matrix{T}) where T <: Real
+````
+Get the normals at each point of the trajectory.
+
+A normal vector is a unit vector orthogonal to the trajectory gradient at each time point
+
+"""
+function get_normals(traj::Matrix{T}) where T <: Real
+	#find the tangential vector at each point
+	grad = diff(traj, dims=1)
+	grad ./= sqrt.(sum(abs2, grad,dims=2))
+	#find the vector orthogonal to the gradient
+	nn = randn(size(grad)...)
+	# remove projection onto grad
+	nn .-= sum(nn.*grad,dims=2).*grad
+	#re-normalize
+	nn ./= sqrt.(sum(abs2,nn,dims=2))
+	@debug "Test" extrema(sum(nn.*grad,dims=2))
+	nn
+end
+"""
+```julia
+function compute_triangular_path_length(traj::Matrix{Float64})
+
+Find the largest combined Euclidean distance from the first point to a point on the trajectory, and from that point to the last point.
+```
+"""
+function compute_triangular_path_length(traj::Matrix{T},method=:normal) where T <: Real
+	nn = size(traj,1)
+	dm = -Inf
+	qidx = 0
+	if method == :normal
+		func = compute_triangular_path_length
+	elseif  method == :sq
+		func = compute_triangular_path_length_sq
+	else
+		error("Unknown method $method")
+	end
+	for i in 2:nn-1
+		_d = func(traj, i)
+		if _d > dm
+			dm = _d
+			qidx = i
+		end
+	end
+	dm,qidx
+end
+
+"""
+```
+function compute_triangular_path_length2(traj::Matrix{Float64})
+
+Return the path length of 3 points that best preserves the overall path length
+```
+"""
+function compute_triangular_path_length2(traj::Matrix{T}) where T <: Real
+	nn = size(traj,1)
+	pl = sum(sqrt.(sum(abs2, diff(traj,dims=1),dims=2)))
+	qidx = 0
+	dm = Inf
+	dq = 0.0
+	for i in 2:nn-1
+		dd = compute_triangular_path_length(traj, i)
+		_dm = dd - pl
+		_dm *= _dm
+		if _dm < dm
+			dm = _dm
+			qidx = i
+			dq = dd
+		end
+	end
+	dq,qidx
+end
+
+function compute_triangular_path_length(traj::Matrix{T},i::Int64) where T <: Real 
+	_d = sqrt(sum(abs2, traj[i,:]  - traj[1,:]))
+	_d += sqrt(sum(abs2, traj[i,:] - traj[end,:]))
+end
+
+"""
+```
+function compute_triangular_path_length_sq(traj::Matrix{Float64},i::Int64)
+````
+Computes path length by summing up the square of the line-elements.
+"""
+function compute_triangular_path_length_sq(traj::Matrix{T},i::Int64) where T <: Real
+	_d = sum(abs2, traj[i,:]  - traj[1,:])
+	_d += sum(abs2, traj[i,:] - traj[end,:])
+end
+
+function compute_triangular_path_length(X::Array{T,3}, qidx::Matrix{Int64}, args...) where T <: Real
+	nn = qidx[2,:] - qidx[1,:] .+ 1
+	path_lengths = fill(0.0, length(nn))
+	for i in 1:length(path_lengths)
+		path_lengths[i],_ = compute_triangular_path_length(X[qidx[1,i]:qidx[2,i],i,:],args...)
+	end
+	path_lengths
+end
+
+function compute_triangular_path_length2(X::Array{T,3}, qidx::Matrix{Int64}) where T <: Real
+	nn = qidx[2,:] - qidx[1,:] .+ 1
+	path_lengths = fill(0.0, length(nn))
+	for i in 1:length(path_lengths)
+		path_lengths[i],_ = compute_triangular_path_length2(X[qidx[1,i]:qidx[2,i],i,:])
+	end
+	path_lengths
+end
+
+function get_path_length_and_rtime(subject::String, t0::Real, t1::Real, ;operation=:path_length, rtmin=120.0, rtmax=300.0, area="FEF", kvs...)
+	fname = joinpath("data", "ppsth_cue_new.jld2")
+	ppstht, labelst, rtimest, trialidxt = JLD2.load(fname, "ppsth","labels", "rtimes", "trialidx")
+	bins = ppstht.bins
+	subject_index = findall(cell->DPHT.get_level_name("subject", cell)==subject, ppstht.cellnames)
+    cellidx = get_area_index(ppstht.cellnames[subject_index], area)
+    cellidx = subject_index[cellidx]
+
+    all_sessions = DPHT.get_level_path.("session", ppstht.cellnames[cellidx])
+    sessions = unique(all_sessions)
+	lrt = Float64[]
+	path_length = Float64[]
+	ncells = fill(0, length(sessions))
+	counts = Float64[]
+	qridx = Int64[]
+	countstats = Dict()
+	offset = 0
+	for (sessionidx,session) in enumerate(sessions)
+		X, labels, rtimes = get_session_data(session, ppstht, trialidxt, labelst, rtimest, cellidx;rtime_min=rtmin, rtime_max=rtmax, mean_subtract=true, variance_stabilize=true)
+		ncells[sessionidx] = size(X,3)
+		# how many locations?
+		nc = maximum(labels)
+		ulabels = unique(labels)
+		sort!(ulabels)
+		nlabels = length(ulabels)
+		for (ii,loc) in enumerate(locations[subject])
+			Xl = X[:,labels.==loc, :]
+			rtimel = rtimes[labels.==loc]
+			qidx = get_transition_period(bins, rtimel, t0, t1)
+
+			tridx = findall(qidx[2,:] - qidx[1,:] .+ 1 .>= 3)
+			for jj in tridx
+				push!(counts, mean(sum(abs2, Xl[qidx[1,jj]:qidx[2,jj],jj,:],dims=1)))
+				qv = countmap(Xl[qidx[1,jj]:qidx[2,jj],jj,:][:])
+				merge!(countstats, qv)
+			end
+			if operation == :path_length
+				S = compute_triangular_path_length(Xl[:, tridx,:], qidx[:,tridx])
+			elseif operation == :path_length2
+				S = compute_triangular_path_length(Xl[:, tridx,:], qidx[:,tridx],:sq)
+			elseif operation == :mean_speed
+				S = fill(0.0, length(tridx))
+				for jj in 1:length(S)
+					trjj = tridx[jj]
+					S[jj] = mean(sqrt.(sum(abs2, diff(Xl[qidx[1, trjj]:qidx[2,trjj],trjj,:],dims=1),dims=2)))
+				end
+			else
+				error("Unkown operation $(operation). Only ``:path_length`` and ``mean_speed`` are currently recognised")
+			end
+			S ./= ncells[sessionidx]
+			if any(isnan.(S))
+				@warn "Nan encountered" sessionidx loc
+			end
+			# subtract the global mean. This probably means that points will shift upwards, since the previous mean,
+			# which did not include the very short reaction time trials, would result in a higher value mean.  In others, now, we are 
+			# subtracting a lower value, which shifts the points upwards.
+			_lrt = log.(rtimel[tridx]) .- mean(log.(rtimel))
+			append!(lrt,_lrt)
+			append!(path_length, S)
+			append!(qridx, offset .+ tridx)
+			offset += size(Xl,2)
+		end
+	end
+	path_length, lrt, qridx
+end
+
+"""
+```julia
+function get_transition_period(bins::AbstractVector{Float64}, rtime::AbstractVector{Float64}, t0::Float64, t1::Float64) where T <: Real
+```
+Get the start and end index of the transition period for each trial
+"""
+function get_transition_period(bins::AbstractVector{Float64}, rtime::AbstractVector{Float64}, t0::Float64, t1::Float64)
+	ntrials = length(rtime)
+	qidx = fill(0, 2, ntrials)
+	idx0 = searchsortedfirst(bins, t0)
+	qidx[1,:] .= idx0
+	for i in 1:ntrials
+		rt = rtime[i]
+		idx1 = searchsortedlast(bins, rt-t1)
+		qidx[2,i] = idx1
+	end
+	qidx
+end
