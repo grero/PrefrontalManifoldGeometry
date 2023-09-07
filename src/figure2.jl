ad92138a43fb57eba937d7a1fb26f592fd029306
Roger Herikstad <roger.herikstad@gmail.com>
2023-09-07 12:39:52 +0800

    Add code for single cell examples

--- src/figure2.jl
+++ src/figure2.jl
@@ -864,3 +864,57 @@ function plot(;do_save=false)
         fig
     end
 end
+
+visual_cells = [("W", 45),("W", 40)] 
+movement_cells = [("W", 54),("J", 22)]
+visuo_movement_cells = [("W", 8),("J", 53)]
+post_saccadic_cells = [("W", 93), ("J", 5)]
+movement_decreasing_cells = [("W", 51),("J", 39)]
+go_cue_cells = [("J", 92), ("J", 59)]
+
+plot_single_cell_examples(windowsize::Real;kvs...) = plot_single_cell_examples([("W", 45),
+                                                                                ("W", 40),
+                                                                                ("W", 54),
+                                                                                ("J", 22),
+                                                                                ("W", 8),
+                                                                                ("J", 53)],
+                                                                                windowsize;kvs...)
+
+function plot_single_cell_examples(cells::Vector{Tuple{String, Int64}}, windowsize;labels::Vector{String}=["A","B","C"], kvs...)
+    width = 15*2.5*72
+    height = width
+    with_theme(plot_theme) do
+        fig = Figure(resolution=(width,height))
+
+        # visual cells
+        lg11 = GridLayout()
+        fig[1,1] = lg11
+        plot_psth_and_raster!(lg11, cells[1][1], cells[1][2],windowsize;xlabelvisible=false, xticklabelsvisible=false, kvs...)
+
+        lg12 = GridLayout()
+        fig[1,2] = lg12
+        plot_psth_and_raster!(lg12, cells[2][1], cells[2][2],windowsize;ylabelvisible=false, xlabelvisible=false, xticklabelsvisible=false, kvs...)
+
+        # movement cells
+        lg21 = GridLayout()
+        fig[2,1] = lg21
+        plot_psth_and_raster!(lg21, cells[3][1], cells[3][2],windowsize;ylabelvisible=true, xlabelvisible=false, xticklabelsvisible=false, kvs...)
+
+        lg22 = GridLayout()
+        fig[2,2] = lg22
+        plot_psth_and_raster!(lg22, cells[4][1], cells[4][2],windowsize;ylabelvisible=false, xlabelvisible=false, xticklabelsvisible=false, kvs...)
+
+        # visuo-movement cells
+        lg31 = GridLayout()
+        fig[3,1] = lg31
+        plot_psth_and_raster!(lg31, cells[5][1], cells[5][2],windowsize;kvs...)
+
+        lg32 = GridLayout()
+        fig[3,2] = lg32
+        plot_psth_and_raster!(lg32, cells[6][1], cells[6][2],windowsize;ylabelvisible=false, kvs...)
+        lmargin = (20,20,20,20)
+        labels = [Label(fig[i,1, TopLeft()], label, padding=lmargin) for (i,label) in enumerate(labels)]
+        fig
+    end
+end
+end # module
