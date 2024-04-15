6e5774a92f4aa9b0f82b3eecf6db956819774c25
Roger Herikstad <roger.herikstad@gmail.com>
2024-04-15 15:01:21 +0800

    Add supplementary figure 1 and 5 to the main module.

--- src/PrefrontalManifoldGeometry.jl
+++ src/PrefrontalManifoldGeometry.jl
@@ -10,4 +10,8 @@ include("figure2.jl")
 include("figure3.jl")
 include("figure4.jl")
 include("figure5.jl")
+
+# supplementaru
+include("supplementary_figure1.jl")
+include("supplementary_figure5.jl")
 end
