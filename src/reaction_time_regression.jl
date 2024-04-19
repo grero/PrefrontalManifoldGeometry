f27e14189ffad81839737ad529b59b2db34e1ad2
Roger Herikstad <roger.herikstad@gmail.com>
2024-04-19 13:33:42 +0800

    Some code to analyze a full regression model

--- src/reaction_time_regression.jl
+++ src/reaction_time_regression.jl
@@ -3,6 +3,8 @@ using JLD2
 using LinearRegressionUtils
 using Distributions
 using CairoMakie
+using Makie: Colors
+using Colors
 
 include("utils.jl")
 include("trajectories.jl")
@@ -33,7 +35,7 @@ end
 
 Get data for regressing reaction time for each point in time for the specified `subject` and `area`.
 """
-function get_regression_data(subject;area="fef", rtmin=120.0, rtmax=300.0, window=35.0, Δt=15.0,align=:mov, realign=true, raw=false, do_shuffle=false, nruns=100,smooth_window::Union{Nothing, Float64}=nothing, kvs...)
+function get_regression_data(subject;area="fef", rtmin=120.0, rtmax=300.0, window=35.0, Δt=15.0,align=:mov, realign=true, raw=false, do_shuffle=false, nruns=100,smooth_window::Union{Nothing, Float64}=nothing, use_midpoint=false,kvs...)
     if raw
         fname = joinpath("data","ppsth_$(area)_$(align)_raw.jld2")
     else 
@@ -91,13 +93,15 @@ function get_regression_data(subject;area="fef", rtmin=120.0, rtmax=300.0, windo
                     continue
                 end
                 # transition period
-                # midpoint
-                ip = div(idxs-idxq+1,2)+1
-
                 Xs = X[idxq:idxs,j2,:]
-                # point of high energy
-                ee = dropdims(sum(abs2, Xs,dims=2),dims=2)
-                ip = argmax(ee)
+                if use_midpoint
+                    # midpoint
+                    ip = div(idxs-idxq+1,2)+1
+                else
+                    # point of high energy
+                    ee = dropdims(sum(abs2, Xs,dims=2),dims=2)
+                    ip = argmax(ee)
+                end
                 if smooth_window !== nothing
                     Xs = mapslices(x->Utils.gaussian_smooth(x, bins[idxq:idxs], smooth_window), Xs, dims=1)
                 end
@@ -113,26 +117,55 @@ function get_regression_data(subject;area="fef", rtmin=120.0, rtmax=300.0, windo
     Z[1:offset,:], L[1:offset,:], rt, label, ncells, bins
 end
 
-"""
+function compute_regression(nruns::Int64, args...)
+    nt = size(args[1],1)
+    trialidx = fill(0, nt, nruns) 
+    for i in 1:nruns
+        trialidx[:,i] = rand(1:nt,nt)
+    end
+    compute_regression(trialidx,args...)...,trialidx
+end 
 
+function compute_regression(trialidx::Matrix{Int64}, args...)
+    nruns = size(trialidx,2)
+    nt = size(args[1],1)
+    tidx = rand(1:nt,nt)
+    _β,_Δβ, _pv, _r² = compute_regression(args...;trialidx=trialidx[:,1])
+    nvars,nbins = size(_β)
+    β = fill(0.0, nvars,nbins,nruns)
+    Δβ = fill(0.0, nvars,nbins,nruns)
+    pv = fill(0.0, nbins,nruns)
+    r² = fill(0.0, nbins, nruns)
+    β[:,:,1] = _β
+    Δβ[:,:,1] = _Δβ
+    pv[:,1] = _pv
+    r²[:,1] = _r² 
+    for i in 2:nruns
+        tidx = rand(1:nt,nt)
+        β[:,:,i],Δβ[:,:,i],pv[:,i],r²[:,i] = compute_regression(args...;trialidx=trialidx[:,i])
+    end
+    β,Δβ, pv, r²
+end
+
+"""
 Compute
 """
-function compute_regression(Z::Matrix{Float64}, L::Matrix{Float64}, xpos::Vector{Float64}, ypos::Vector{Float64}, ncells::Vector{Int64}, rt::Vector{Float64},bins::AbstractVector{Float64})
+function compute_regression(Z::Matrix{Float64}, L::Matrix{Float64}, xpos::Vector{Float64}, ypos::Vector{Float64}, ncells::Vector{Int64}, rt::Vector{Float64},bins::AbstractVector{Float64};trialidx=1:size(Z,1))
     nbins = size(Z,2)
     β = fill(NaN, 5, nbins)
     Δβ = fill(NaN, 5, nbins)
     pv = fill(NaN, nbins)
     r² = fill(NaN, nbins)
     for i in axes(Z,2) 
-        if !all(isfinite.(L[:,i]))
+        if !all(isfinite.(L[trialidx,i]))
           continue
         end
         # run two regression models; one without path length L and one with
-        X_no_L = [Z[:,i] xpos ypos ncells]
-        X_with_L = [Z[:,i] xpos ypos L[:,i] ncells] 
+        X_no_L = [Z[trialidx,i] xpos[trialidx] ypos[trialidx] ncells[trialidx]]
+        X_with_L = [Z[trialidx,i] xpos[trialidx] ypos[trialidx] L[trialidx,i] ncells[trialidx]] 
 
-        lreg_no_L = LinearRegressionUtils.llsq_stats(X_no_L, rt;do_interactions=true, exclude_pairs=[(2,3)])
-        lreg_with_L = LinearRegressionUtils.llsq_stats(X_with_L, rt;do_interactions=true, exclude_pairs=[(2,3)])
+        lreg_no_L = LinearRegressionUtils.llsq_stats(X_no_L, rt[trialidx];do_interactions=true, exclude_pairs=[(2,3)])
+        lreg_with_L = LinearRegressionUtils.llsq_stats(X_with_L, rt[trialidx];do_interactions=true, exclude_pairs=[(2,3)])
 
         # compute the F-stat for whether adding the path length results in a significantly better fit
         n = length(rt)
@@ -140,9 +173,6 @@ function compute_regression(Z::Matrix{Float64}, L::Matrix{Float64}, xpos::Vector
         p2 = length(lreg_with_L.β)
         rss1 = lreg_no_L.rss
         p1 = length(lreg_no_L.β)
-        if i == 1
-            @show p1 p2 rss1 rss2 lreg_with_L.varidx
-        end
         F = (rss1 - rss2)/(p2-p1)
         F /= rss2/(n-p2)
         pv[i] = 1.0 - cdf(FDist(p2-p1, n-p2), F)
@@ -182,14 +212,17 @@ function plot_regression(β, Δβ,pv,r²,bins)
     end
 end
 
-function plot_fef_dlpfc_r²()
-    fname = "path_length_regression_fef_dlpfc_comparison.jld2"
+function plot_fef_dlpfc_r²(;tt=65.0,nruns=100, use_midpoint=false)
+    fname = "path_length_regression_fef_dlpfc_comparison_with_stats.jld2"
+    if use_midpoint
+        fname = replace(fname, "with_stats"=>"midpoint_with_stats")
+    end
     if isfile(fname)
         qdata = JLD2.load(fname)
     else
         qdata = Dict()
         for subject in ["J","W"]
-            Z,L,lrt,label,ncells,bins = get_regression_data(subject;area="fef", align=:cue,raw=true, mean_subtract=true, variance_stabilize=true,window=50.0,smooth_window=nothing);
+            Z,L,lrt,label,ncells,bins = get_regression_data(subject;area="fef", align=:cue,raw=true, mean_subtract=true, variance_stabilize=true,window=50.0,smooth_window=nothing,use_midpoint=use_midpoint);
             if subject == "J"
                 tidx = findall(label.!=9)
             else
@@ -197,36 +230,53 @@ function plot_fef_dlpfc_r²()
             end
             xpos = [p[1] for p in Utils.location_position[subject]][label[tidx]]
             ypos = [p[2] for p in Utils.location_position[subject][label[tidx]]]
-            βfef,Δβfef,pvfef,r²fef = compute_regression(Z[tidx,:], L[tidx,:], xpos, ypos,ncells[tidx],lrt[tidx],bins)
+            βfef,Δβfef,pvfef,r²fef,trialidx = compute_regression(nruns,Z[tidx,:], L[tidx,:], xpos, ypos,ncells[tidx],lrt[tidx],bins)
 
-            Z,L,lrt,label,ncells,bins = get_regression_data(subject;area="dlpfc", align=:cue,raw=true, mean_subtract=true, variance_stabilize=true,window=50.0,smooth_window=nothing);
-            βdlpfc,Δβdlpfc,pvdlpfc,r²dlpfc = compute_regression(Z[tidx,:], L[tidx,:], xpos, ypos,ncells[tidx],lrt[tidx],bins)
+            Z,L,lrt,label,ncells,bins = get_regression_data(subject;area="dlpfc", align=:cue,raw=true, mean_subtract=true, variance_stabilize=true,window=50.0,smooth_window=nothing, use_midpoint=use_midpoint);
+            βdlpfc,Δβdlpfc,pvdlpfc,r²dlpfc = compute_regression(trialidx, Z[tidx,:], L[tidx,:], xpos, ypos,ncells[tidx],lrt[tidx],bins)
             qdata[subject] = Dict("βfef"=>βfef, "Δβfef"=>Δβfef,"pvfef"=>pvfef, "r²fef"=>r²fef,
                                   "βdlpfc"=>βdlpfc, "Δβdlpfc"=>Δβdlpfc, "pvdlpfc"=>pvdlpfc, "r²dlpfc"=>r²dlpfc,
-                                  "bins"=>bins)
+                                  "bins"=>bins,"trialidx"=>trialidx)
         end
         JLD2.save(fname, qdata)
     end
+    colors = [PlotUtils.fef_color, PlotUtils.dlpfc_color]
+    fcolors = [RGB(0.5 + 0.5*c.r, 0.5+0.5*c.g, 0.5+0.5*c.b) for c in colors]
     with_theme(PlotUtils.plot_theme) do
         fig = Figure(size=(500,500))
         axes = [Axis(fig[i,1]) for i in 1:4]
         for (ii,subject) in enumerate(["J","W"])
             bins,r²fef, r²dlpfc,βfef, Δβfef,βdlpfc, Δβdlpfc = [qdata[subject][k] for k in ["bins", "r²fef","r²dlpfc","βfef","Δβfef","βdlpfc","Δβdlpfc"]]
             bidx = findall(isfinite, r²fef)
+            idxtt = searchsortedfirst(bins,tt)
             ax1 = axes[(ii-1)*2+1]
-            vlines!(ax1, 0.0, color=:black)
-            lines!(ax1, bins, r²fef, label="FEF")
-            lines!(ax1, bins, r²dlpfc, label="DLPFC")
+            @show size(r²fef)
+            mid_fef = dropdims(median(r²fef,dims=2),dims=2)
+            mid_dlpfc = dropdims(median(r²dlpfc,dims=2),dims=2)
+            bidx = findall(isfinite, mid_fef)
+            lower_fef,upper_fef = [mapslices(x->percentile(x,pc), r²fef[bidx,:],dims=2) for pc in [5,95]]
+            lower_dlpfc,upper_dlpfc = [mapslices(x->percentile(x,pc), r²dlpfc[bidx,:],dims=2) for pc in [5,95]]
+            band!(ax1, bins[bidx], lower_fef[:], upper_fef[:],color=fcolors[1])
+            lines!(ax1, bins, mid_fef, label="FEF",color=PlotUtils.fef_color)
+            band!(ax1, bins[bidx], lower_dlpfc[:], upper_dlpfc[:],color=fcolors[2])
+            lines!(ax1, bins, mid_dlpfc, label="DLPFC",color=PlotUtils.dlpfc_color)
+
             ax1.title="Monkey $subject"
             ax1.ylabel = "r²"
             ax2 = axes[ii*2]
             ax2.ylabel = "β"
-            lines!(ax2, bins[bidx], βfef[1,bidx], label="FEF")
-            band!(ax2, bins[bidx], (βfef-2Δβfef)[1,bidx], (βfef+2Δβfef)[1,bidx])
-            lines!(ax2, bins[bidx], βdlpfc[1,bidx], label="DLPFC")
-            band!(ax2, bins[bidx], (βdlpfc-2Δβdlpfc)[1,bidx], (βdlpfc+2Δβdlpfc)[1,bidx])
-            hlines!(ax2, 0.0, color=:black, linestyle=:dot)
+            lower_fef,mid_fef, upper_fef = [mapslices(x->percentile(x,pc), βfef[1,bidx,:],dims=2) for pc in [5,50,95]]
+            lower_dlpfc,mid_dlpfc, upper_dlpfc = [mapslices(x->percentile(x,pc), βdlpfc[1,bidx,:],dims=2) for pc in [5,50,95]]
 
+            band!(ax2, bins[bidx], lower_fef[:], upper_fef[:],color=fcolors[1])
+            lines!(ax2, bins[bidx], mid_fef[:], label="FEF",color=colors[1])
+            hlines!(ax2, mid_fef[idxtt],color=colors[1])
+            band!(ax2, bins[bidx], lower_dlpfc[:], upper_dlpfc[:],color=fcolors[2])
+            lines!(ax2, bins[bidx], mid_dlpfc[:], label="DLPFC",color=colors[2])
+            hlines!(ax2, 0.0, color=:black, linestyle=:dot)
+        end
+        for ax in axes
+            vlines!(ax, [0.0,tt], color=[:black,:gray])
         end
         linkaxes!(axes[1],axes[3])
         linkxaxes!(axes...)
@@ -338,30 +388,64 @@ function plot_regression(Z::Matrix{Float64}, L::Matrix{Float64}, xpos::Vector{Fl
     end
 end
 
-function plot_β_summary()
-    fname = "path_length_regression_fef_dlpfc_comparison.jld2"
+function plot_β_summary(;use_midpoint=false)
+    fname = "path_length_regression_fef_dlpfc_comparison_with_stats.jld2"
+    if use_midpoint
+        fname = replace(fname, "with_stats"=>"midpoint_with_stats")
+    end
     if isfile(fname)
         qdata = JLD2.load(fname)
     end
     μ = Dict()
+    σ² = Dict()
     for (ii,subject) in enumerate(["J","W"])
         bins, βfef, Δβfef,βdlpfc, Δβdlpfc = [qdata[subject][k] for k in ["bins","βfef","Δβfef","βdlpfc","Δβdlpfc"]]
         idx0 = searchsortedfirst(bins,-300.0)        
         idx1 = searchsortedfirst(bins, -150.0)
         idx2 = searchsortedfirst(bins, 65);
-        μdlpfc = abs(βdlpfc[1,idx2])/abs(mean(βdlpfc[1,idx0:idx1]))
-        μfef = abs(βfef[1,idx2])/abs(mean(βfef[1,idx0:idx1]))
-        μ[subject] = Dict("dlpfc"=>μdlpfc,"fef"=>μfef)
+        # TODO: Use the standard deviations of the beta to estalish significance
+        μ[subject] = Dict("dlpfc"=>Dict("transition"=>βdlpfc[1,idx2,:], "baseline"=>dropdims(mean(βdlpfc[1,idx0:idx1,:],dims=1),dims=1)),
+                          "fef"=>Dict("transition"=>βfef[1,idx2,:], "baseline"=>dropdims(mean(βfef[1,idx0:idx1,:],dims=1),dims=1)))
     end
 
     colors = [PlotUtils.fef_color, PlotUtils.dlpfc_color]
+    fcolors = [RGB(0.5 + 0.5*c.r, 0.5+0.5*c.g, 0.5+0.5*c.b) for c in colors]
+    acolors = [colors;fcolors]
     with_theme(PlotUtils.plot_theme) do
         fig = Figure()
-        ax = Axis(fig[1,1])
+        ax1 = Axis(fig[1,1])
+        Yt = [μ["J"]["fef"]["transition"] μ["J"]["dlpfc"]["transition"] μ["W"]["fef"]["transition"] μ["W"]["dlpfc"]["transition"]]
+        Yb = [μ["J"]["fef"]["baseline"] μ["J"]["dlpfc"]["baseline"] μ["W"]["fef"]["baseline"] μ["W"]["dlpfc"]["baseline"]]
 
-        barplot!(ax, 1:4, [μ["J"]["fef"], μ["J"]["dlpfc"],μ["W"]["fef"],μ["W"]["dlpfc"]],
+        lower_b,mid_b,upper_b = [dropdims(mapslices(x->percentile(x,pp),Yb,dims=1),dims=1) for pp in [5,50,95]]
+        lower_t,mid_t,upper_t = [dropdims(mapslices(x->percentile(x,pp),Yt,dims=1),dims=1) for pp in [5,50,95]]
+        mid = [mid_t[1],mid_b[1],mid_t[2],mid_b[2],mid_t[3],mid_b[3],mid_t[4],mid_b[4]]
+        lower = [lower_t[1],lower_b[1],lower_t[2],lower_b[2],lower_t[3],lower_b[3],lower_t[4],lower_b[4]]
+        upper = [upper_t[1],upper_b[1],upper_t[2],upper_b[2],upper_t[3],upper_b[3],upper_t[4],upper_b[4]]
+        barplot!(ax1, 1:8,mid,
+                          color=acolors[[1,3, 2,4, 1,3,2,4]])
+       
+        rangebars!(ax1, 1:8, lower, upper)
+        ax1.ylabel = "β"
+        Legend(fig[1,1], [PolyElement(color=RGB(0.0, 0.0,0.0)),
+                           PolyElement(color=RGB(0.5, 0.5, 0.5))],
+                           ["Transition","Baseline"],tellwidth=false,
+                           valign=:top)
+        ax1.xticks = [2.5, 7.0]
+        ax1.xticklabelsvisible = false
+        ax = Axis(fig[2,1])
+
+        # ratio
+        func(x) = reduce(/, abs.(x))
+
+        lower,mid,upper = [dropdims(mapslices(x->percentile(x,pp),abs.(Yt./Yb),dims=1),dims=1) for pp in [5,50,95]]
+
+        barplot!(ax, 1:4,mid,
                           color=colors[[1,2,1,2]])
-        Legend(fig[1,1], [PolyElement(color=colors[i]) for i in 1:2],["FEF", "DLPFC"],
+
+        rangebars!(ax, 1:4, lower,upper)
+        @show lower[[1,3]] upper[[2,4]] lower[[2,4]]
+        Legend(fig[2,1], [PolyElement(color=colors[i]) for i in 1:2],["FEF", "DLPFC"],
                 valign=:top, halign=:left, tellwidth=false)
         ax.ylabel = "β transition/β baseline"
         ax.xticks=([1.5, 3.5],["Monkey J","Monkey W"])
