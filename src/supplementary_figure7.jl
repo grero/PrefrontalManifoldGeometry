cfa4993cbdec46e6cbed00744e7724663d147cec
Roger Herikstad <roger.herikstad@gmail.com>
2024-05-16 11:32:29 +0800

    Tweak Figure S7

--- src/supplementary_figure7.jl
+++ src/supplementary_figure7.jl
@@ -2,9 +2,10 @@
 Go-cue onset and movement onset spaces in DLPFC
 """
 module FigureS7
-include("figure2.jl")
+using JLD2
+using ..Figure2
 
-function plot(;do_save=true, kvs...)
+function plot(;do_save=false, kvs...)
     fname_cue, fname_mov = Figure2.get_event_subspaces(;subject="ALL", rtime_min=120.0,area="DLPFC")
     fig = Figure2.plot_event_onset_subspaces(fname_cue, fname_mov;max_latency=70.0, kvs...)
     if do_save
