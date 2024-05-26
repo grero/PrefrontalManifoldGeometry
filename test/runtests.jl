99d8e599d1a8d2fd85e99719e6b22f6782bafdeb
Roger Herikstad <roger.herikstad@gmail.com>
2024-05-26 12:57:04 +0800

    Add test for regression

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,19 @@
 using Test
+using TestItems
 using PrefrontalManifoldGeometry
+using StableRNGs
 const PMG = PrefrontalManifoldGeometry
+
+@testset "Regression" begin
+    @testitem "Regression" begin
+        rng = StableRNG(1234)
+        xt = randn(rng, 300,1)
+        y = 0.1.*xt[:,1] .+ 0.3
+        z = randn(rng, 300,1)
+        β, Δβ, pv, r², rss, varidx = PrefrontalManifoldGeometry.Figure3.compute_regression(lrt, xt, z;use_residuals=true, save_all_β=true)
+        @show pv
+    end
+end
 
 @testset "Figures" begin
     fig1 = PMG.Figure1().plot()
