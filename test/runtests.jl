82274e8e06d88d2a68ec5072721fc768712487bc
Roger Herikstad <roger.herikstad@gmail.com>
2024-09-24 15:08:18 +0800

    Add tests for responsive and selective cells

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,22 @@ using PrefrontalManifoldGeometry
 using StableRNGs
 const PMG = PrefrontalManifoldGeometry
 
+@testset "Responsive and selective" begin
+    nt = 20
+    nbins = 3
+    X = fill(0.0, nbins, nt)
+    label = rand(1:2, nt)
+    n1 = sum(label.==1)
+    n2 = sum(label.==2)
+    X[1,:] .= 0.1*randn(nt)
+    X[2,label.==1] .= 1.0 .+ 0.1*randn(n1)
+    X[2,label.==2] .= 0.5 .+ 0.1*randn(n2)
+    X[3,label.==1] .= 0.5 .+ 0.1*randn(n1)
+    X[3,label.==2] .= 1.0 .+ 0.1*randn(n2)
+    is_responsive, is_selective = PMG.Figure2.classify_cell(X, [0,1,2], label, [(0,1),(1,2), (2,3)])
+    @test is_responsive == [true, true]
+    @test is_selective == [true, true]
+end
 @testset "Regression" begin
     @testitem "Regression" begin
         rng = StableRNG(1234)
