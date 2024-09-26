b0ca9092398bdff8dd0cfdb7f28a6fe82400c412
Roger Herikstad <roger.herikstad@gmail.com>
2024-09-26 10:08:59 +0800

    fix path issue

--- src/figure5.jl
+++ src/figure5.jl
@@ -525,7 +525,7 @@ function plot_bimodal_analysis(;redo=false, nruns=100,plot_loglikelihood=false)
 	rtime_mid = _sdata_mid["rtime_stim"][classified_saccades_mid.rtidx.stim]
 	rtime_nostim = [_sdata_early["rtime_nostim"][classified_saccades_early.rtidx.nostim];_sdata_mid["rtime_nostim"][classified_saccades_mid.rtidx.nostim]]
 
-	fname = joinpath(@__DIR__, "data","figure5_bimodel_analysis.jld2")
+	fname = joinpath(@__DIR__, "..", "data","figure5_bimodel_analysis.jld2")
 	if !redo && isfile(fname)
 		model_bic, n_modes, mode_assignment, model_converged,model,loglike,loglike_nostim = JLD2.load(fname, "model_bic",
 																			    "n_modes",
